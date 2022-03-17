f37c4ddd4e203cb3416bc988448a60564e0159cc
Allan Araujo Pinheiro <apinheiro@railz.ai>
2022-03-16 22:31:03 -0300

    chore: more unit tests helpers

--- packages/components/src/helpers/test/chart.utils.spec.ts
+++ packages/components/src/helpers/test/chart.utils.spec.ts
@@ -1,9 +1,24 @@
-import { getConfiguration } from '../chart.utils';
+import {
+  RVAccountingProviders,
+  RVDateFilters,
+  RVFilter,
+  RVOptions,
+  RVReportFrequency,
+  RVReportTypes,
+} from '../../types';
+import {
+  getConfiguration,
+  getDateFilter,
+  parseFilter,
+  getOptions,
+  validateBusinessParams,
+  validateReportFrequencyParams,
+  validateRequiredParams,
+} from '../chart.utils';
 
-// TODO: complete tests
 describe('Chart Utils Helper', () => {
   describe('getConfiguration', () => {
-    describe('getConfiguration using non object', () => {
+    describe('using non object', () => {
       describe('failure path', () => {
         test('returns undefined due to integer passed in string', async () => {
           expect(getConfiguration('1')).toBeUndefined();
@@ -39,7 +54,7 @@ describe('Chart Utils Helper', () => {
         });
       });
     });
-    describe('getConfiguration using object', () => {
+    describe('using object', () => {
       describe('failure path', () => {
         test('returns undefined due to empty configuration', async () => {
           expect(getConfiguration({ token: '' })).toBeUndefined();
@@ -48,6 +63,293 @@ describe('Chart Utils Helper', () => {
       describe('success path', () => {
         test('returns formatted data due to the presence of configuration', async () => {
           expect(getConfiguration({ token: 'er2' })).toStrictEqual({ token: 'er2' });
+        });
+      });
+    });
+  });
+
+  describe('parseFilter', () => {
+    describe('using non object', () => {
+      describe('failure path', () => {
+        test('returns undefined due to integer passed in string', async () => {
+          expect(parseFilter('1')).toBeUndefined();
+        });
+
+        test('returns undefined due to plain string passed', async () => {
+          expect(parseFilter('testing param')).toBeUndefined();
+        });
+
+        test('returns undefined due to empty stringified array with passed', async () => {
+          expect(parseFilter('[]')).toBeUndefined();
+        });
+
+        test('returns undefined due to stringified array passed', async () => {
+          expect(parseFilter('["data"]')).toBeUndefined();
+        });
+
+        test('returns undefined due to empty stringified object passed', async () => {
+          expect(parseFilter('{}')).toBeUndefined();
+        });
+
+        test('returns undefined due to stringified object passed without filter', async () => {
+          expect(parseFilter('{"tom": "works"}')).toBeUndefined();
+        });
+      });
+
+      describe('success path', () => {
+        test('returns formatted data due to the presence of filter', async () => {
+          const filter: RVFilter = {
+            reportType: RVReportTypes.BANK_ACCOUNT,
+            businessName: 'businessName',
+            serviceName: RVAccountingProviders.QUICKBOOKS,
+          };
+
+          expect(parseFilter(filter)).toStrictEqual({
+            businessName: 'businessName',
+            reportType: 'bankAccounts',
+            serviceName: 'quickbooks',
+          });
+        });
+      });
+    });
+    describe('using object', () => {
+      describe('failure path', () => {
+        test('returns undefined due to stringified object passed without businessName', async () => {
+          const filter: RVFilter = {
+            reportType: RVReportTypes.BANK_ACCOUNT,
+            businessName: '',
+            serviceName: RVAccountingProviders.QUICKBOOKS,
+          };
+
+          expect(parseFilter(JSON.stringify(filter))).toBeUndefined();
+        });
+      });
+
+      describe('success path', () => {
+        test('returns formatted data due to the presence of filter', async () => {
+          const filter: RVFilter = {
+            reportType: RVReportTypes.BANK_ACCOUNT,
+            businessName: 'businessName',
+            serviceName: RVAccountingProviders.QUICKBOOKS,
+          };
+
+          expect(parseFilter(JSON.stringify(filter))).toStrictEqual({
+            businessName: 'businessName',
+            reportType: 'bankAccounts',
+            serviceName: 'quickbooks',
+          });
+        });
+      });
+    });
+  });
+
+  describe('validateRequiredParams', () => {
+    describe('success path', () => {
+      test('returns true to valid params (businessName/serviceName)', async () => {
+        const filter: RVFilter = {
+          reportType: RVReportTypes.BANK_ACCOUNT,
+          businessName: 'businessName',
+          serviceName: RVAccountingProviders.QUICKBOOKS,
+        };
+        expect(validateRequiredParams(filter)).toEqual(true);
+      });
+
+      test('returns true to valid params (connectionId)', async () => {
+        const filter: RVFilter = {
+          reportType: RVReportTypes.BANK_ACCOUNT,
+          connectionId: 'connectionId',
+        };
+        expect(validateRequiredParams(filter)).toEqual(true);
+      });
+    });
+  });
+
+  describe('validateReportFrequencyParams', () => {
+    describe('success path', () => {
+      test('returns true for correct params', async () => {
+        const filter: RVFilter = {
+          reportType: RVReportTypes.BALANCE_SHEET,
+          businessName: '',
+          serviceName: RVAccountingProviders.QUICKBOOKS,
+          reportFrequency: RVReportFrequency.MONTH,
+          startDate: '2020-01-01',
+          endDate: '2020-01-01',
+        };
+        expect(validateReportFrequencyParams(filter)).toEqual(true);
+      });
+    });
+  });
+
+  describe('validateBusinessParams', () => {
+    describe('failure path', () => {
+      test('returns false if there is not businessName', async () => {
+        const filter: RVFilter = {
+          reportType: RVReportTypes.BANK_ACCOUNT,
+          businessName: '',
+          serviceName: RVAccountingProviders.QUICKBOOKS,
+        };
+        expect(validateBusinessParams(filter)).toEqual(false);
+      });
+      test('returns false if there is not businessName', async () => {
+        const filter: RVFilter = {
+          reportType: RVReportTypes.BANK_ACCOUNT,
+          connectionId: '',
+        };
+        expect(validateBusinessParams(filter)).toEqual(false);
+      });
+    });
+
+    describe('success path', () => {
+      test('returns true for businessName and serviceName', async () => {
+        const filter: RVFilter = {
+          reportType: RVReportTypes.BANK_ACCOUNT,
+          businessName: 'businessNames',
+          serviceName: RVAccountingProviders.QUICKBOOKS,
+        };
+        expect(validateBusinessParams(filter)).toEqual(true);
+      });
+
+      test('returns true for connectionId', async () => {
+        const filter: RVFilter = {
+          reportType: RVReportTypes.BANK_ACCOUNT,
+          connectionId: 'connectionId',
+        };
+        expect(validateBusinessParams(filter)).toEqual(true);
+      });
+    });
+  });
+
+  describe('getOptions', () => {
+    describe('failure path', () => {
+      test('returns undefined due to plain string passed', async () => {
+        expect(getOptions('testing param')).toBeUndefined();
+      });
+    });
+
+    describe('success path', () => {
+      test('returns formatted data due to the presence of filter', async () => {
+        const options: RVOptions = {
+          container: {
+            style: {},
+          },
+          title: {
+            text: 'title',
+            style: {},
+          },
+          chart: {},
+          bar: {},
+          loadingIndicator: {},
+          errorIndicator: {},
+        };
+        expect(getOptions(options)).toEqual({
+          bar: {},
+          chart: {},
+          container: { style: {} },
+          errorIndicator: {},
+          loadingIndicator: {},
+          title: { style: {}, text: 'title' },
+        });
+      });
+    });
+  });
+
+  describe('getDateFilter', () => {
+    describe('using non object', () => {
+      describe('failure path', () => {
+        test('returns undefined due to integer passed in string', async () => {
+          expect(getDateFilter('1')).toBeUndefined();
+        });
+
+        test('returns undefined due to plain string passed', async () => {
+          expect(getDateFilter('testing param')).toBeUndefined();
+        });
+
+        test('returns undefined due to empty stringified array with passed', async () => {
+          expect(getDateFilter('[]')).toBeUndefined();
+        });
+
+        test('returns undefined due to stringified array passed', async () => {
+          expect(getDateFilter('["data"]')).toBeUndefined();
+        });
+
+        test('returns undefined due to empty stringified object passed', async () => {
+          expect(getDateFilter('{}')).toBeUndefined();
+        });
+
+        test('returns undefined due to stringified object passed without filter', async () => {
+          expect(getDateFilter('{"tom": "works"}')).toBeUndefined();
+        });
+
+        test('returns undefined for the same date', async () => {
+          const filter: RVDateFilters = {
+            reportType: RVReportTypes.BALANCE_SHEET,
+            businessName: 'businessName',
+            serviceName: RVAccountingProviders.QUICKBOOKS,
+            reportFrequency: RVReportFrequency.MONTH,
+            startDate: '2020-01-01',
+            endDate: '2020-01-01',
+          };
+          expect(getDateFilter(filter)).toBeUndefined();
+        });
+
+        test('returns undefined for startDate after than endDate', async () => {
+          const filter: RVDateFilters = {
+            reportType: RVReportTypes.BALANCE_SHEET,
+            businessName: 'businessName',
+            serviceName: RVAccountingProviders.QUICKBOOKS,
+            reportFrequency: RVReportFrequency.MONTH,
+            startDate: '2020-01-02',
+            endDate: '2020-01-01',
+          };
+          expect(getDateFilter(filter)).toBeUndefined();
+        });
+      });
+
+      describe('success path', () => {
+        test('returns formatted data due to the presence of filter', async () => {
+          const filter: RVDateFilters = {
+            reportType: RVReportTypes.BALANCE_SHEET,
+            businessName: 'businessName',
+            serviceName: RVAccountingProviders.QUICKBOOKS,
+            reportFrequency: RVReportFrequency.MONTH,
+            startDate: '2020-01-01',
+            endDate: '2020-01-02',
+          };
+          expect(getDateFilter(filter)).toStrictEqual({
+            businessName: 'businessName',
+            reportType: RVReportTypes.BALANCE_SHEET,
+            serviceName: 'quickbooks',
+            reportFrequency: RVReportFrequency.MONTH,
+            startDate: '2020-01-01',
+            endDate: '2020-01-02',
+          });
+        });
+      });
+    });
+    describe('using object', () => {
+      describe('failure path', () => {
+        test('returns undefined due to stringified object passed without businessName', async () => {
+          // const filter: RVFilter = {
+          //   reportType: RVReportTypes.BANK_ACCOUNT,
+          //   businessName: '',
+          //   serviceName: RVAccountingProviders.QUICKBOOKS,
+          // };
+          // expect(parseFilter(JSON.stringify(filter))).toBeUndefined();
+        });
+      });
+
+      describe('success path', () => {
+        test('returns formatted data due to the presence of filter', async () => {
+          // const filter: RVFilter = {
+          //   reportType: RVReportTypes.BANK_ACCOUNT,
+          //   businessName: 'businessName',
+          //   serviceName: RVAccountingProviders.QUICKBOOKS,
+          // };
+          // expect(parseFilter(JSON.stringify(filter))).toStrictEqual({
+          //   businessName: 'businessName',
+          //   reportType: 'bankAccounts',
+          //   serviceName: 'quickbooks',
+          // });
         });
       });
     });
--- packages/components/src/helpers/test/utils.spec.ts
+++ packages/components/src/helpers/test/utils.spec.ts
@@ -1,4 +1,12 @@
-import { formatDate, getTitleByReportType, formatSeries } from '../utils';
+import {
+  formatDate,
+  getTitleByReportType,
+  formatSeries,
+  formatNumber,
+  isStatements,
+  isTransactions,
+  isRequiredReportFrequency,
+} from '../utils';
 import Translations from '../../config/translations/en.json';
 import { RVBalanceSheetSummary, RVReportFrequency, RVReportTypes } from '../../types';
 
@@ -98,6 +106,137 @@ describe('Utils Helper Tests', () => {
           data: [456, 456],
           name: 'name',
         });
+      });
+    });
+  });
+
+  describe('formatNumber', () => {
+    describe('null values', () => {
+      test('returns empty when sent null values', async () => {
+        expect(formatNumber(null, null)).toEqual('');
+      });
+    });
+    describe('happy day', () => {
+      test('returns number on the correct format', async () => {
+        expect(formatNumber(1000000, 2)).toEqual('1,000,000');
+        expect(formatNumber(1000000, 4)).toEqual('1,000,000');
+        expect(formatNumber(1000000)).toEqual('1,000,000');
+        expect(formatNumber(1)).toEqual('1');
+        expect(formatNumber(-1000)).toEqual('-1,000');
+        expect(formatNumber(-100000000)).toEqual('-100,000,000');
+        expect(formatNumber(1000.999, 1)).toEqual('1,001');
+        expect(formatNumber(1000.123, 2)).toEqual('1,000.12');
+        expect(formatNumber(1000.1, 4)).toEqual('1,000.1');
+        expect(formatNumber(123.000001, 5)).toEqual('123');
+        expect(formatNumber(123.00001, 5)).toEqual('123.00001');
+        expect(formatNumber(0.1)).toEqual('0.1');
+        expect(formatNumber(-100000000.123, 3)).toEqual('-100,000,000.123');
+      });
+    });
+  });
+
+  describe('isStatements', () => {
+    describe('null values', () => {
+      test('returns empty when sent null values', async () => {
+        expect(isStatements(null)).toEqual(null);
+      });
+    });
+    describe('happy day', () => {
+      test('returns false for type that is statement', async () => {
+        expect(isStatements(RVReportTypes.BALANCE_SHEET)).toEqual(true);
+        expect(isStatements(RVReportTypes.INCOME_STATEMENTS)).toEqual(true);
+        expect(isStatements(RVReportTypes.CASHFLOW_STATEMENTS)).toEqual(true);
+      });
+
+      test('returns false for type that is not statement', async () => {
+        expect(isStatements(RVReportTypes.BANK_ACCOUNT)).toEqual(false);
+        expect(isStatements(RVReportTypes.BILLS)).toEqual(false);
+        expect(isStatements(RVReportTypes.CREDIT_SCORE)).toEqual(false);
+        expect(isStatements(RVReportTypes.EXPENSES)).toEqual(false);
+        expect(isStatements(RVReportTypes.FINANCIAL_RATIO)).toEqual(false);
+        expect(isStatements(RVReportTypes.INVOICES)).toEqual(false);
+        expect(isStatements(RVReportTypes.REVENUE)).toEqual(false);
+      });
+    });
+  });
+
+  describe('isTransactions', () => {
+    describe('null values', () => {
+      test('returns empty when sent null values', async () => {
+        expect(isTransactions(null)).toEqual(null);
+      });
+    });
+    describe('happy day', () => {
+      test('returns false for type that is transactions', async () => {
+        expect(isTransactions(RVReportTypes.INVOICES)).toEqual(true);
+        expect(isTransactions(RVReportTypes.BILLS)).toEqual(true);
+      });
+
+      test('returns false for type that is not transactions', async () => {
+        expect(isTransactions(RVReportTypes.BALANCE_SHEET)).toEqual(false);
+        expect(isTransactions(RVReportTypes.INCOME_STATEMENTS)).toEqual(false);
+        expect(isTransactions(RVReportTypes.CASHFLOW_STATEMENTS)).toEqual(false);
+        expect(isTransactions(RVReportTypes.BANK_ACCOUNT)).toEqual(false);
+        expect(isTransactions(RVReportTypes.CREDIT_SCORE)).toEqual(false);
+        expect(isTransactions(RVReportTypes.EXPENSES)).toEqual(false);
+        expect(isTransactions(RVReportTypes.FINANCIAL_RATIO)).toEqual(false);
+        expect(isTransactions(RVReportTypes.REVENUE)).toEqual(false);
+      });
+    });
+  });
+
+  describe('getTitleByReportType', () => {
+    describe('null values', () => {
+      test('returns empty when sent null values', async () => {
+        expect(getTitleByReportType(null)).toEqual('');
+      });
+    });
+    describe('happy day', () => {
+      test('returns translated title for type', async () => {
+        expect(getTitleByReportType(RVReportTypes.INVOICES)).toEqual(Translations.RV_INVOICES);
+        expect(getTitleByReportType(RVReportTypes.BILLS)).toEqual(Translations.RV_BILLS);
+        expect(getTitleByReportType(RVReportTypes.BALANCE_SHEET)).toEqual(
+          Translations.RV_BALANCE_SHEETS,
+        );
+        expect(getTitleByReportType(RVReportTypes.INCOME_STATEMENTS)).toEqual(
+          Translations.INCOME_STATEMENTS,
+        );
+        expect(getTitleByReportType(RVReportTypes.CASHFLOW_STATEMENTS)).toEqual(
+          Translations.CASHFLOW_STATEMENTS,
+        );
+      });
+
+      test('returns empty when the type has no translation', async () => {
+        expect(getTitleByReportType(RVReportTypes.BANK_ACCOUNT)).toEqual('');
+        expect(getTitleByReportType(RVReportTypes.CREDIT_SCORE)).toEqual('');
+        expect(getTitleByReportType(RVReportTypes.EXPENSES)).toEqual('');
+        expect(getTitleByReportType(RVReportTypes.FINANCIAL_RATIO)).toEqual('');
+        expect(getTitleByReportType(RVReportTypes.REVENUE)).toEqual('');
+      });
+    });
+  });
+
+  describe('isRequiredReportFrequency', () => {
+    describe('null values', () => {
+      test('returns empty when sent null values', async () => {
+        expect(isRequiredReportFrequency(null)).toEqual(null);
+      });
+    });
+    describe('happy day', () => {
+      test('returns false for type that is required to have report frequency', async () => {
+        expect(isRequiredReportFrequency(RVReportTypes.REVENUE)).toEqual(true);
+        expect(isRequiredReportFrequency(RVReportTypes.EXPENSES)).toEqual(true);
+        expect(isRequiredReportFrequency(RVReportTypes.CASHFLOW_STATEMENTS)).toEqual(true);
+        expect(isRequiredReportFrequency(RVReportTypes.BALANCE_SHEET)).toEqual(true);
+        expect(isRequiredReportFrequency(RVReportTypes.INCOME_STATEMENTS)).toEqual(true);
+        expect(isRequiredReportFrequency(RVReportTypes.FINANCIAL_RATIO)).toEqual(true);
+      });
+
+      test('returns false for type that is not required to have report frequency', async () => {
+        expect(isRequiredReportFrequency(RVReportTypes.BANK_ACCOUNT)).toEqual(false);
+        expect(isRequiredReportFrequency(RVReportTypes.BILLS)).toEqual(false);
+        expect(isRequiredReportFrequency(RVReportTypes.CREDIT_SCORE)).toEqual(false);
+        expect(isRequiredReportFrequency(RVReportTypes.INVOICES)).toEqual(false);
       });
     });
   });
