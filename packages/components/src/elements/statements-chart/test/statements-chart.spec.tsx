222bb5ac3ea50ed2254b273feb60233a32682087
abisolaadeniran <88153326+abisolaadeniran@users.noreply.github.com>
2022-03-31 17:13:59 -0300

    chore: cleanup packages (#31)
    * chore: update scripts
    
    * chore: include package-manager
    
    * chore: fix tests

--- packages/components/src/elements/statements-chart/test/statements-chart.spec.tsx
+++ packages/components/src/elements/statements-chart/test/statements-chart.spec.tsx
@@ -1,67 +1,39 @@
 import { newSpecPage } from '@stencil/core/testing';
 
 import { StatementsChart } from '../statements-chart';
-// TODO: write tests
-describe.skip('statements-chart', () => {
-  it('renders empty without parameter', async () => {
-    const page = await newSpecPage({
-      components: [StatementsChart],
-      html: `<railz-statements-chart></railz-statements-chart>`,
-    });
-    expect(page.root).toEqualHtml(`
+// TODO: add success path tests
+describe('statements-chart', () => {
+  describe('Fail to render due to wrong parameters', () => {
+    it('renders error without parameter', async () => {
+      const page = await newSpecPage({
+        components: [StatementsChart],
+        html: `<railz-statements-chart></railz-statements-chart>`,
+      });
+      expect(page.root).toEqualHtml(`
       <railz-statements-chart>
         <mock:shadow-root>
-            <div class="global-div">
-              <div></div>
+            <div class="railz-container">
+              <railz-error-image statuscode="500"></railz-error-image>
             </div>
         </mock:shadow-root>
       </railz-statements-chart>
     `);
-  });
-  it('renders header with title parameter', async () => {
-    const page = await newSpecPage({
-      components: [StatementsChart],
-      html: `<railz-statements-chart options='{title: "Cashflow Statements"}'></railz-statements-chart>`,
     });
-    expect(page.root).toEqualHtml(`
-      <railz-statements-chart>
+    it('renders error with title parameter', async () => {
+      const page = await newSpecPage({
+        components: [StatementsChart],
+        html: `<railz-statements-chart options='{title: "Cashflow Statements"}'></railz-statements-chart>`,
+      });
+      expect(page.root).toEqualHtml(`
+      <railz-statements-chart options='{title: "Cashflow Statements"}'>
         <mock:shadow-root>
-            <div class="global-div">
-                <p class="title">Cashflow Statements</p>
+            <div class="railz-container">
+                <railz-error-image statuscode="500"></railz-error-image>
             </div>
         </mock:shadow-root>
       </railz-statements-chart>
     `);
-  });
-  it('renders header with error parameter', async () => {
-    const page = await newSpecPage({
-      components: [StatementsChart],
-      html: `<railz-statements-chart error="true"></railz-statements-chart>`,
     });
-    expect(page.root).toEqualHtml(`
-      <railz-statements-chart>
-        <mock:shadow-root>
-            <div class="global-div">
-              <railz-error-image error="true" statuscode="500"></railz-error-image>
-            </div>
-        </mock:shadow-root>
-      </railz-statements-chart>
-    `);
   });
-  it('renders header with all valid parameter', async () => {
-    const page = await newSpecPage({
-      components: [StatementsChart],
-      html: `<railz-statements-chart error="true"></railz-statements-chart>`,
-    });
-    expect(page.root).toEqualHtml(`
-      <railz-statements-chart>
-        <mock:shadow-root>
-            <div class="global-div">
-                <p class="title">Cashflow Statements</p>
-                <railz-error-image error="true" statuscode="500"></railz-error-image>
-            </div>
-        </mock:shadow-root>
-      </railz-statements-chart>
-    `);
-  });
+  describe.skip('Render successfully due to correct parameters', () => {});
 });
