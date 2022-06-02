fa0fb5a629e97c7da943fbd87385262fcdf5c46c
Allan <apinheiro@railz.ai>
2022-06-02 16:21:03 -0300

    chore: Updating bank accounts to only plaid

--- packages/components/src/elements/bank-accounts/bank-accounts.utils.ts
+++ packages/components/src/elements/bank-accounts/bank-accounts.utils.ts
@@ -3,9 +3,10 @@ import { pick } from 'lodash-es';
 import Translations from '../../config/translations/en.json';
 import { RVFormattedBankAccountsResponse, RVParams, RVReportTypesUrlMapping } from '../../types';
 import { RequestServiceInstance } from '../../services/request';
-import { errorLog } from '../../services/logger';
+import { errorLog, warnLog } from '../../services/logger';
 
 import { RVReportRequestParameter } from './../../types/interface/summary/parameters';
+import { RVAllProviders } from './../../types/enum/service-providers';
 
 /**
  * Make API call based on expected parameters for bank account data type
@@ -14,10 +15,19 @@ export const getReportData = async ({
   filter,
 }: RVReportRequestParameter): Promise<RVFormattedBankAccountsResponse> => {
   let reportData;
+  //TODO START: This should be changed when we have more banks
+  if (filter.serviceName !== RVAllProviders.PLAID) {
+    warnLog(Translations.RV_BANK_ACCOUNT_MUST_BE_SERVICE_NAME_PLAID);
+  }
+  const pickedFilter = {
+    ...pick(filter, [RVParams.BUSINESS_NAME]),
+    serviceName: RVAllProviders.PLAID,
+  };
+  //TODO END
   try {
     reportData = await RequestServiceInstance.getReportData({
       path: RVReportTypesUrlMapping[filter.reportType],
-      filter: pick(filter, [RVParams.SERVICE_NAME, RVParams.BUSINESS_NAME]),
+      filter: pickedFilter,
     });
   } catch (error) {
     errorLog(Translations.NOT_ABLE_TO_RETRIEVE_REPORT_DATA, error);
