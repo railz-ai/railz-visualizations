8abe12376649480c73b136df4c7e44c1ca5c25c8
Santiago Persico <spersico@railz.ai>
2024-03-27 21:19:01 +0100

    chore: tidy up environment variables

--- examples/nodejs/index.js
+++ examples/nodejs/index.js
@@ -10,12 +10,10 @@ Check the README.md for more information on how to set these variables
 const AUTH_URL = process.env.RAILZ_AUTH_URL || 'https://auth.railz.ai/getAccess';
 const CLIENT_ID = process.env.RAILZ_CLIENT_ID;
 const CLIENT_SECRET = process.env.RAILZ_CLIENT_SECRET;
-
-//initialize express
-const app = express();
-
-//use env port or assign 4000
 const PORT = process.env.PORT || 4000;
+
+// initialize express
+const app = express();
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
 
@@ -28,7 +26,6 @@ app.use(function (req, res, next) {
   next();
 });
 
-// Main endpoint
 app.get('/', (request, response) => {
   if (!AUTH_URL || !CLIENT_ID || !CLIENT_SECRET) {
     const err = 'Environment variables not properly set, see documentation';
