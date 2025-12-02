8632373a87c2da602ca3dcf5f0e7e2c2ae51a358
afawaz-railz <106553595+afawaz-railz@users.noreply.github.com>
2025-12-02 17:47:01 -0500

    Feature/bps 74089 visualize historical forecasted data (#187)
    * feat: New component financial forecasts

--- packages/components/src/elements/financial-forecasts/financial-forecasts.utils.ts
+++ packages/components/src/elements/financial-forecasts/financial-forecasts.utils.ts
@@ -1,6 +1,6 @@
 import { isNil, pick } from 'lodash-es';
 
-import { format, parseISO, startOfMonth } from 'date-fns';
+import { format, parseISO, startOfMonth, subDays } from 'date-fns';
 
 import Translations from '../../config/translations/en.json';
 import { formatDate, formatSeries, isStatements } from '../../helpers/utils';
@@ -309,16 +309,11 @@ export const getReportData = async ({
       ['startDate', 'endDate', 'reportFrequency', 'connectionUuid'],
     );
     // Compute beginning of current month as both Date and formatted string for safe comparisons and API params
-    const beginningOfCurrentMonthDate = startOfMonth(new Date());
-    const beginningOfCurrentMonth = format(beginningOfCurrentMonthDate, RAILZ_DATE_FORMAT);
+    const beginningOfCurrentMonth = format(startOfMonth(new Date()), RAILZ_DATE_FORMAT);
 
     const historicalDataParams = {
       ...dataParams,
       // Use the earlier of the provided startDate and the beginning of the current month
-      startDate:
-        parseISO(filter.startDate) < beginningOfCurrentMonthDate
-          ? startDate
-          : beginningOfCurrentMonth,
       // Set end date to the beginning of the current month
       endDate: beginningOfCurrentMonth,
     };
@@ -333,10 +328,6 @@ export const getReportData = async ({
       filter: {
         ...dataParams,
         startDate: beginningOfCurrentMonth,
-        endDate:
-          parseISO(filter.endDate) > beginningOfCurrentMonthDate
-            ? endDate
-            : beginningOfCurrentMonth,
         financialStatementType: filter.financialStatementType,
         percentile: filter?.percentile,
         macro: 'fis-reconstruct',
@@ -356,10 +347,10 @@ export const combineHistoricalAndForecastedData = (
 ): RVFormattedStatementData => {
   // if either historical or forecasted data is missing, return the other one
   if (!historicalData?.series || historicalData?.series.length === 0) {
-    return forecastedData;
+    return { ...forecastedData, xPlotLineValue: 0 };
   }
   if (!forecastedData?.series || forecastedData?.series.length === 0) {
-    return historicalData;
+    return { ...historicalData, xPlotLineValue: historicalData.categories.length };
   }
 
   const combinedCategories = [...historicalData?.categories, ...forecastedData?.categories];
